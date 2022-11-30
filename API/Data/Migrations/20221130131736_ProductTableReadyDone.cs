22720cb75980b4370158d2ba037041b0d572bc9f
Nir Simkin <nir.simkin@gmail.com>
2022-11-30 15:37:33 +0200

    Created Table Products and Api Controller.

---
+++ API/Data/Migrations/20221130131736_ProductTableReadyDone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductTableReadyDone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
